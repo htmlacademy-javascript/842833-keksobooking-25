6d328de77dfa53a4e83451118902ae9aaddc7241
Darya <stolyarova.1995@gmail.com>
2022-03-13 10:38:56 +0300

    задание4

--- js/main.js
+++ js/main.js
@@ -7,17 +7,6 @@ const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/ke
 const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const times = ['12:00', '13:00', '14:00'];
 
-const getInteger = (fromInteger, upToInteger) => {
-  if (fromInteger < 0 || upToInteger < 0) {
-
-    return 'невозжожно выполнить';
-  }
-
-  fromInteger = Math.floor(fromInteger);
-  upToInteger = Math.floor(upToInteger);
-  return Math.floor(Math.random() * (fromInteger - upToInteger + 1)) + upToInteger;
-};
-
 const getFractionalNumbers = (fromFractionalNumber, upToFractionalNumber, fraction = 1) => {
   if (fromFractionalNumber < 0 || upToFractionalNumber < 0) {
 
@@ -27,6 +16,17 @@ const getFractionalNumbers = (fromFractionalNumber, upToFractionalNumber, fracti
   fromFractionalNumber = Math.floor(fromFractionalNumber);
   upToFractionalNumber = Math.floor(upToFractionalNumber);
   return (Math.random() * (fromFractionalNumber - upToFractionalNumber + 1) + upToFractionalNumber).toFixed(fraction);
+};
+
+const getInteger = (fromInteger, upToInteger) => {
+  if (fromInteger < 0 || upToInteger < 0) {
+
+    return 'невозжожно выполнить';
+  }
+
+  fromInteger = Math.floor(fromInteger);
+  upToInteger = Math.floor(upToInteger);
+  return Math.floor(Math.random() * (fromInteger - upToInteger + 1)) + upToInteger;
 };
 
 const getRandomArray = (element) => {
